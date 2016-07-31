e8295e57cfc692b49e579b88d1425521ca16b019
AustinDev16 <austin.blaser@gmail.com>
2016-07-31 15:59:11 -0600

    Hooked up API and tested it

--- RestaurantFinder/RestaurantListController.swift
+++ RestaurantFinder/RestaurantListController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+class RestaurantListController: UITableViewController {
 
+    let coordinate = Coordinate(latitude: 40.759106, longitude: -73.985185)
+    let foursquareClient = FoursquareClient(clientID: "EPGEGJ5DFVYTGOG1R2DXU325IMO2MKNXRVS1PLX4ZBJDYSVK", clientSecret: "31R3WXRULOPL3JDY0KJ21IQLFXO20AJ11XUCDE24GMSYLVNF")
+    
+    var venues: [Venue] = []{
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     var detailViewController: DetailViewController? = nil
     var objects = [AnyObject]()
 
@@ -17,13 +26,14 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        foursquareClient.fetchRestaurantsFor(coordinate, category: .Food(nil))
+        { result in
+            switch result {
+            case .Success(let venues):
+                self.venues = venues
+            case .Failure(let error):
+                print(error)
+            }
         }
     }
 
@@ -37,20 +47,16 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+  
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -64,14 +70,15 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return venues.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        
+        let venue = venues[indexPath.row]
+        cell.textLabel?.text = venue.name
+        
         return cell
     }
 
@@ -80,14 +87,7 @@ class MasterViewController: UITableViewController {
         return true
     }
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
+
 
 
 }
